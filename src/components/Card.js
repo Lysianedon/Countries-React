bcc322bd0f6f1e5da38a6d4c83cf3c2a5ce210be
lysiandon <don.lysiane@gmail.com>
2022-02-04 00:27:08 +0100

    transfered style to App.css + added a search bar

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,32 @@
 import React from 'react'
 
+
+
+
 class Card extends React.Component {
 
 
     render(){
 
-        return (<div style={{border: "1px solid black", 
-        height : "50vh", 
-        width: "20%", 
-        display:"flex", 
-        alignItems:"center", 
-        justifyContent:"space-evenly", 
-        flexDirection : "column"}}>
+        return (
+        <div style={{border: "2px solid grey", 
+                            height : "60vh",
+                            borderRadius : "3%", 
+                            width: "20em",
+                            display:"flex",
+                            flexDirection:"column",
+                            alignItems:"center", 
+                            justifyContent:"space-evenly", 
+                            boxShadow: "1px 2px 8px 2px #9CA6A4",
+                            margin:"2% auto"}}>
 
-            {this.props.name}
-            {this.props.capital}
-            {this.props.flag}
-            {this.props.population}
-            {this.props.region}
+           
+           <img src={this.props.flag} style={{maxWidth: "60%", margin:"5%"}} alt="drapeau pays"></img>
+           <h1>Country : {this.props.name} </h1> 
+           <h2>Capital : {this.props.capital} </h2>
+           <p>Population : {this.props.population}</p>
+           <p>Region : {this.props.region}</p>
+  
 
         </div>)
     }
