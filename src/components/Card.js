4d8510721efbad7ff30f73874465d44343951bd0
lysiandon <don.lysiane@gmail.com>
2022-02-15 11:15:05 +0100

    commit json

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 
-
-
 class Card extends React.Component {
-
 
     render(){
 
         return (
         <div style={{border: "2px solid grey", 
-                            height : "60vh",
+                            height : "63vh",
                             borderRadius : "3%", 
                             width: "20em",
                             display:"flex",
@@ -20,8 +17,10 @@ class Card extends React.Component {
                             boxShadow: "1px 2px 8px 2px #9CA6A4",
                             margin:"2% auto"}}>
 
-           
-           <img src={this.props.flag} style={{maxWidth: "60%", margin:"5%"}} alt="drapeau pays"></img>
+           <img src={this.props.flag} 
+           style={{maxWidth: "60%", margin:"5%"}} 
+           alt="drapeau pays"></img>
+
            <h1>Country : {this.props.name} </h1> 
            <h2>Capital : {this.props.capital} </h2>
            <p>Population : {this.props.population}</p>
